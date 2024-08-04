198eaa71a8bb3766d559ede8c4d8940604b901cc
Ryan Leyhe <ryanleyhe8@gmail.com>
2024-08-04 12:31:06 -0400

    refactor files and folders to separate frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dataRoute = require("./routes/data.route.js")
 const app = express()
+const cors = require('cors')
 require('dotenv').config();
 
 app.use("/api/data", dataRoute);
