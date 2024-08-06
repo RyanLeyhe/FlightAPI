3c0f67c303886f4867b19610a229a56150d4346a
Ryan Leyhe <ryanleyhe8@gmail.com>
2024-08-05 22:13:09 -0400

    change vite config to host on network

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/FlightAPI/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // This makes the server accessible on all network interfaces
+    port: 5173, // Ensure this port matches the port Vite is using
+  }
 })
